d630e3ae00244af9047e44b73bd6a60be8ac63b4
merykul <mariakulv@gmail.com>
2023-02-10 11:41:12 +0300

    removed code which starts app into BaseTestRunner

--- src/test/java/androidTests/AppiumBasics.java
+++ src/test/java/androidTests/AppiumBasics.java
@@ -1,34 +1,13 @@
 package androidTests;
 
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.AppiumBy;
 import org.testng.annotations.Test;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class AppiumBasics{
+public class AppiumBasics extends BaseTestRunner {
 
     @Test
-    public void appiumTest() throws MalformedURLException {
-//         Automated start of Appium server [not working, running onto "The local appium server has not been started" error]
-//        AppiumDriverLocalService service = new AppiumServiceBuilder()
-//                .withAppiumJS(new File("C://Users//User//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
-//                .withIPAddress("127.0.0.1")
-//                .usingPort(4723)
-//                .build();
-//        service.start();
+    public void appiumTest() {
+        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
 
-        // Capabilities for Samsung Galaxy A22 and App
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("Samsung");
-        options.setCapability("udid", "adb-R58R62JC02R-SndPFB._adb-tls-connect._tcp");
-        options.setCapability("platformName", "Android");
-        options.setCapability("platformVersion", "12");
-        options.setApp("C://Users//User//IdeaProjects//appium_android//src//test//java//resourses//ApiDemos-debug.apk");
-
-        URL url = new URL("http://127.0.0.1:4723/wd/hub");
-        AndroidDriver driver = new AndroidDriver(url, options);
-        driver.quit();
     }
 }
