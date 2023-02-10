855716c18c85f04a08a390bb26c0a8ec2b9d3fc0
merykul <mariakulv@gmail.com>
2023-02-10 18:20:14 +0300

    added timeouts

--- src/test/java/androidTests/BaseTestRunner.java
+++ src/test/java/androidTests/BaseTestRunner.java
@@ -3,9 +3,9 @@ package androidTests;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.testng.annotations.*;
-
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 public class BaseTestRunner {
     public AndroidDriver driver;
@@ -21,6 +21,7 @@ public class BaseTestRunner {
 
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
         driver = new AndroidDriver(url, options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @AfterMethod
