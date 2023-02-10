9a541415fa1d194403be24662ac2683577e0f0b9
merykul <mariakulv@gmail.com>
2023-02-10 11:41:34 +0300

    fixed

--- src/test/java/androidTests/CalculatorTest.java
+++ src/test/java/androidTests/CalculatorTest.java
@@ -3,23 +3,30 @@ package androidTests;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import utils.BaseTestRunner;
-
 import java.net.MalformedURLException;
+import java.net.URL;
 
-public class CalculatorTest extends BaseTestRunner {
+public class CalculatorTest {
     static AppiumDriver driver;
 
-    @BeforeClass
-    public void setUp() throws MalformedURLException {
-        startApp();
-    }
-
     @Test
-    public static void addOperation() {
+    public static void addOperation() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "Samsung");
+        capabilities.setCapability("udid", "adb-R58R62JC02R-SndPFB._adb-tls-connect._tcp");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "12");
+
+        capabilities.setCapability("appPackage", "com.sec.android.app.popupcalculator");
+        capabilities.setCapability("appActivity", "com.sec.android.app.popupcalculator.Calculator");
+
+
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+        driver = new AppiumDriver(url, capabilities);
+
         WebElement seven = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_07"));
         WebElement plus = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_add"));
         WebElement zero = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_00"));
