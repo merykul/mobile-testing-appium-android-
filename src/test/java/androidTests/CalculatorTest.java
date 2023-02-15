9a5de37311c323b627ccc9a31b00bfb850cedfa4
merykul <mariakulv@gmail.com>
2023-02-15 22:45:54 +0300

    added before method

--- src/test/java/androidTests/CalculatorTest.java
+++ src/test/java/androidTests/CalculatorTest.java
@@ -5,42 +5,43 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CalculatorTest {
     static AppiumDriver driver;
 
-    @Test
-    public static void addOperation() throws MalformedURLException {
+    @BeforeMethod
+    public void before() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName", "Samsung");
         capabilities.setCapability("udid", "adb-R58R62JC02R-SndPFB._adb-tls-connect._tcp");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("platformVersion", "12");
-
         capabilities.setCapability("appPackage", "com.sec.android.app.popupcalculator");
         capabilities.setCapability("appActivity", "com.sec.android.app.popupcalculator.Calculator");
 
-
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
         driver = new AppiumDriver(url, capabilities);
+    }
 
+    @Test
+    public static void addOperation() {
         WebElement seven = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_07"));
         WebElement plus = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_add"));
         WebElement zero = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_00"));
         WebElement equals = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/calc_keypad_btn_equal"));
-
-         seven.click();;
-         plus.click();
-         zero.click();
-         equals.click();
-
-       WebElement result = driver.findElement(By.className("android.view.ViewGroup"));
-       String res = result.getText();
-       String expectedResult = "7";
-       Assert.assertEquals(res, expectedResult);
+        seven.click();
+        plus.click();
+        zero.click();
+        equals.click();
+        WebElement result = driver.findElement(By.className("android.view.ViewGroup"));
+        String res = result.getText();
+        String expectedResult = "7";
+        Assert.assertEquals(res, expectedResult);
     }
 }
 
