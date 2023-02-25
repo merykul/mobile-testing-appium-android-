71e076b4b8f1b0f5612b7000f2c7b7abb0ed4e38
merykul <mariakulv@gmail.com>
2023-02-25 15:58:32 +0300

    modified wifiSettingsName() test with adding android keys and proceeding directly to the activity

--- src/test/java/androidTests/AppiumBasics.java
+++ src/test/java/androidTests/AppiumBasics.java
@@ -1,8 +1,13 @@
 package androidTests;
 
+import apps_pages.EntryPage;
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
@@ -13,19 +18,27 @@ public class AppiumBasics extends BaseTestRunner {
 
     @Test
     public void wifiSettingsName() {
-        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-        driver.findElement(AppiumBy.accessibilityId("3. Preference dependencies")).click();
+        Activity activity = new Activity("d88026b u0 io.appium.android.apis", "io.appium.android.apis.preference.PreferenceDependencies");
+        driver.startActivity(activity);
+
         driver.findElement(AppiumBy.id("android:id/checkbox")).click();
+        DeviceRotation landScape = new DeviceRotation(0, 0, 90);
+        driver.rotate(landScape);
         driver.findElement(AppiumBy.xpath("/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.RelativeLayout/android.widget.TextView")).click();
         String alert = driver.findElement(AppiumBy.id("android:id/alertTitle")).getText();
         Assert.assertEquals(alert, "WiFi settings");
-        driver.findElement(AppiumBy.id("android:id/edit")).sendKeys("MeryWiFi");
+        driver.setClipboardText("MeryWiFi");
+        driver.findElement(AppiumBy.id("android:id/edit")).sendKeys(driver.getClipboardText());
+        driver.pressKey(new KeyEvent(AndroidKey.ENTER));
         driver.findElement(AppiumBy.id("android:id/button1")).click();
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));
+        driver.pressKey(new KeyEvent(AndroidKey.HOME));
     }
 
     @Test
     public void longPressGesture() {
-        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        new EntryPage(driver).clickViews();
+
         driver.findElement(AppiumBy.accessibilityId("Expandable Lists")).click();
         driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
         RemoteWebElement peopleNamesButton = (RemoteWebElement) driver
