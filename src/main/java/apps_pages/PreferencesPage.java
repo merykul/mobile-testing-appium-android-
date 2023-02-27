00d0842c33be2af25acd3f238757d29fa04b042e
merykul <mariakulv@gmail.com>
2023-02-27 23:22:25 +0300

    added web elements and methods

--- src/main/java/apps_pages/PreferencesPage.java
+++ src/main/java/apps_pages/PreferencesPage.java
@@ -2,9 +2,20 @@ package apps_pages;
 
 import base.BasePO;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class PreferencesPage extends BasePO{
+
+    @FindBy(xpath = "//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")
+    private WebElement preferenceDependencies;
+
     public PreferencesPage(WebDriver driver) {
         super(driver);
+    }
+
+    public PreferenceDependencies clickPreferenceDependencies() {
+        preferenceDependencies.click();
+        return new PreferenceDependencies(driver);
     }
 }
