ba6d80eccedbb8b36ed8a5631be06d8b8e956b4d
merykul <mariakulv@gmail.com>
2023-02-28 21:42:43 +0300

    modified

--- src/test/java/androidTests/CalculatorTest.java
+++ src/test/java/androidTests/CalculatorTest.java
@@ -1,34 +1,14 @@
 package androidTests;
 
 import calculator.MainPage;
-import io.appium.java_client.AppiumDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.BaseTestRunnerForCalculator;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class CalculatorTest {
-    static AppiumDriver driver;
-
-    @BeforeMethod
-    public void before() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName", "Samsung");
-        capabilities.setCapability("udid", "adb-R58R62JC02R-SndPFB._adb-tls-connect._tcp");
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "12");
-        capabilities.setCapability("appPackage", "com.sec.android.app.popupcalculator");
-        capabilities.setCapability("appActivity", "com.sec.android.app.popupcalculator.Calculator");
-
-        URL url = new URL("http://127.0.0.1:4723/wd/hub");
-        driver = new AppiumDriver(url, capabilities);
-    }
+public class CalculatorTest extends BaseTestRunnerForCalculator{
 
     @Test
-    public static void addOperation() {
+    public void multipleOperation() {
         String result = new MainPage(driver)
                 .clickZero()
                 .clickMultiple()
