86b577b92352ed677419b41e1acefd689c241894
merykul <mariakulv@gmail.com>
2023-02-28 11:03:04 +0300

    modifying test using Page Object pattern

--- src/test/java/androidTests/AppiumBasics.java
+++ src/test/java/androidTests/AppiumBasics.java
@@ -1,13 +1,12 @@
 package androidTests;
 
-import apps_pages.EntryPage;
+import org.openqa.selenium.DeviceRotation;
+import pages.EntryPage;
 import com.google.common.collect.ImmutableMap;
-import components.WiFiSettingsPopUp;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import org.openqa.selenium.By;
-import org.openqa.selenium.DeviceRotation;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
@@ -17,33 +16,28 @@ import org.testng.annotations.Test;
 public class AppiumBasics extends BaseTestRunner {
     @Test
     public void wifiSettingsName() {
-        WiFiSettingsPopUp wiFiSettingsCheck = new EntryPage(driver)
+        driver.setClipboardText("MeryWiFi");
+        new EntryPage(driver)
                 .clickPreferences()
                 .clickPreferenceDependencies()
                 .clickWiFiCheckbox()
-                .clickWiFiSettings();
-
-        String alert = driver.findElement(AppiumBy.id("android:id/alertTitle")).getText();
-        Assert.assertEquals(alert, "WiFi settings");
-        driver.setClipboardText("MeryWiFi");
-        driver.findElement(AppiumBy.id("android:id/edit")).sendKeys(driver.getClipboardText());
-        driver.pressKey(new KeyEvent(AndroidKey.ENTER));
-        driver.findElement(AppiumBy.id("android:id/button1")).click();
+                .clickWiFiSettings()
+                .enterDataIntoInputField(driver.getClipboardText())
+                .clickOkButton();
+        DeviceRotation landScape = new DeviceRotation(0, 0, 90);
+        driver.rotate(landScape);
         driver.pressKey(new KeyEvent(AndroidKey.BACK));
         driver.pressKey(new KeyEvent(AndroidKey.HOME));
-
-//        DeviceRotation landScape = new DeviceRotation(0, 0, 90);
-//        driver.rotate(landScape);
     }
 
     @Test
     public void longPressGesture() {
-        new EntryPage(driver).clickViews();
+        RemoteWebElement peopleNamesButton = new EntryPage(driver)
+                .clickViews()
+                .clickEnableLists()
+                .clickCustomerAdapter()
+                .peopleNameButtonFind();
 
-        driver.findElement(AppiumBy.accessibilityId("Expandable Lists")).click();
-        driver.findElement(AppiumBy.accessibilityId("1. Custom Adapter")).click();
-        RemoteWebElement peopleNamesButton = (RemoteWebElement) driver
-                .findElement(By.xpath("//android.widget.TextView[@text='People Names']"));
         longPressGesture(peopleNamesButton);
         String actualResult = driver.findElement(By.id("android:id/title")).getText();
         Assert.assertEquals(actualResult, "Sample menu");
@@ -51,15 +45,17 @@ public class AppiumBasics extends BaseTestRunner {
 
     @Test
     public void scrollTest() {
-        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        new EntryPage(driver)
+                .clickViews();
         driver.findElement(AppiumBy
                 .androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));"));
     }
 
     @Test
     public void swipeTest() {
-        driver.findElement(AppiumBy.accessibilityId("Views")).click();
-        driver.findElement(AppiumBy.accessibilityId("Gallery")).click();
+        new EntryPage(driver)
+                .clickViews()
+                .clickGallery();
         driver.findElement(AppiumBy.accessibilityId("1. Photos")).click();
         RemoteWebElement widgetImage = (RemoteWebElement) driver.findElement(AppiumBy.xpath("(//android.widget.ImageView)[1]"));
         Assert.assertEquals(widgetImage.getAttribute("focusable"), "true");
@@ -69,8 +65,7 @@ public class AppiumBasics extends BaseTestRunner {
 
     @Test
     public void DragDropTest() throws InterruptedException {
-        driver.findElement(AppiumBy.accessibilityId("Views")).click();
-        driver.findElement(AppiumBy.accessibilityId("Drag and Drop")).click();
+        new EntryPage(driver).clickViews().clickDragAndDropPage();
         WebElement source = driver.findElement(By.id("io.appium.android.apis:id/drag_dot_1"));
         ((JavascriptExecutor) driver).executeScript("mobile: dragGesture", ImmutableMap.of(
                 "elementId", ((RemoteWebElement) source).getId(),
