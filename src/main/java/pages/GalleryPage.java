d7a67ced98a7516c08df5432837e1c5c591d2c87
merykul <mariakulv@gmail.com>
2023-03-04 11:04:50 +0300

    added clickPhotos() method

--- src/main/java/pages/GalleryPage.java
+++ src/main/java/pages/GalleryPage.java
@@ -2,9 +2,20 @@ package pages;
 
 import base.BasePO;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class GalleryPage extends BasePO{
+
+    @FindBy(id = "00000000-0000-0037-ffff-ffff00000038")
+    private WebElement photos;
+
     public GalleryPage(WebDriver driver) {
         super(driver);
+    }
+
+    public PhotosPage clickPhotos() {
+        photos.click();
+        return new PhotosPage(driver);
     }
 }
